f03f90760869501a8de3f5af78ab4fa56359f478
swapnil <swapnils6297@gmail.com>
2026-01-15 01:23:17 +0530

    home page changes done by swapnil

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
 LOGIN_REDIRECT_URL='/home'
-LOGOUT_REDIRECT_URL='login'
+LOGOUT_REDIRECT_URL='/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
